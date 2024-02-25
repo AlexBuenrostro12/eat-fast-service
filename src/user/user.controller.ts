2f5d91b3c2390526a73fbc58d9284c98acee4489
Carlos Buenrostro <c.buenrostro@blackstone.studio>
2024-02-24 18:04:39 -0600

    add logic to user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOneById(@Param('id') id: number) {
+    return this.userService.findOneById(id);
+  }
+
+  @Post()
+  create(@Body() payload: CreateUserDto) {
+    return this.userService.create(payload);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: number, @Body() payload: UpdateUserDto) {
+    return this.userService.update(id, payload);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.userService.delete(id);
+  }
+}

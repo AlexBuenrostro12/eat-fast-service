5ada28829242ac8da21ce202623d0e5aa84d7361
Carlos Buenrostro <c.buenrostro@blackstone.studio>
2024-03-08 17:12:23 -0600

    remove console.log

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -19,7 +19,6 @@ export class AuthController {
   @UseGuards(LocalAuthGuard)
   @Post('login')
   login(@Request() req: any) {
-    console.log('req: ', req);
     return this.authService.login(req.user);
   }
 }
